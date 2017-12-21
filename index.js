0bafca6bd52e73ecc596713e2df0420ad542a14f
sese <re-se@users.noreply.github.com>
2017-12-21 22:19:36 +0900

    initial

---
+++ index.js
@@ -0,0 +1,11 @@
+const visit = require(`unist-util-visit`);
+const breaks = require(`remark-breaks`);
+
+module.exports = ({ markdownAST }) => {
+  visit(markdownAST, `break`, node => {
+    node.type = `html`;
+    node.value = `<br>`;
+  });
+};
+
+module.exports.setParserPlugins = () => [breaks];
